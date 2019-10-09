83ce0059b76166933b8906f6aef449f610122161
Tuyen Le <tuyen@aromajoin.com>
2019-10-09 15:53:29 +0900

    change diffuse method of AromaFlower

--- sample/ControllerSDKSample/ControllerSDKSample/ViewController.swift
+++ sample/ControllerSDKSample/ControllerSDKSample/ViewController.swift
@@ -9,15 +9,11 @@ import CoreBluetooth
 class ViewController: UIViewController {
   let aromaShooterController = AromaShooterController.sharedInstance
   
-  override func viewDidLoad() {
-    aromaShooterController.delegate = self
-  }
-  
   @IBAction func diffuseAroma(_ sender: UIButton) {
     // Work for both AS1 and AS2
     aromaShooterController.diffuseAll(duration: 1000, booster: true, ports: [1,2,3])
 
-    // Only for AS2. Where each catridge port has its own intensity. As oppose to AS1, which does not have intensity
+    // Only for AS2
     aromaShooterController.diffuseAll(durationInMilli: 1000, boosterIntensity: 0, fanIntensity: 50, ports: [CartridgePort(number: 3, intensityPercent: 100)])
   }
   
@@ -30,20 +26,6 @@ class ViewController: UIViewController {
     if let connectionVC = connectionVC {
       self.navigationController?.pushViewController(connectionVC, animated: true)
     }
-  }
-}
-
-extension ViewController: AromaShooterDelegate {
-  func aromaShooter(didDiscoverDevice device: AromaShooter) {
-
-  }
-  
-  func aromaShooter(didConnectDevice device: AromaShooter) {
-
-  }
-  
-  func aromaShooter(didDisconnectDevice device: AromaShooter) {
-  
   }
 }
 
