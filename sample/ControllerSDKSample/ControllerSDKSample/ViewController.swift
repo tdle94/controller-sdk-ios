544bac037c073260fe69186e33dad477b70435d5
Tuyen Le <tuyen@aromajoin.com>
2019-10-09 10:47:21 +0900

    update framework to work with AS2

--- sample/ControllerSDKSample/ControllerSDKSample/ViewController.swift
+++ sample/ControllerSDKSample/ControllerSDKSample/ViewController.swift
@@ -4,12 +4,21 @@
 
 import UIKit
 import AromaShooterControllerSwift
+import CoreBluetooth
 
 class ViewController: UIViewController {
   let aromaShooterController = AromaShooterController.sharedInstance
   
+  override func viewDidLoad() {
+    aromaShooterController.delegate = self
+  }
+  
   @IBAction func diffuseAroma(_ sender: UIButton) {
-    aromaShooterController.diffuseAll(duration: 2000, booster: true, ports: [sender.tag])
+    // Work for both AS1 and AS2
+    aromaShooterController.diffuseAll(duration: 1000, booster: true, ports: [1,2,3])
+
+    // Only for AS2. Where each catridge port has its own intensity. As oppose to AS1, which does not have intensity
+    aromaShooterController.diffuseAll(durationInMilli: 1000, boosterIntensity: 0, fanIntensity: 50, ports: [CartridgePort(number: 3, intensityPercent: 100)])
   }
   
   @IBAction func stopDiffusing(_ sender: UIButton) {
@@ -21,6 +30,20 @@ class ViewController: UIViewController {
     if let connectionVC = connectionVC {
       self.navigationController?.pushViewController(connectionVC, animated: true)
     }
+  }
+}
+
+extension ViewController: AromaShooterDelegate {
+  func aromaShooter(didDiscoverDevice device: AromaShooter) {
+
+  }
+  
+  func aromaShooter(didConnectDevice device: AromaShooter) {
+
+  }
+  
+  func aromaShooter(didDisconnectDevice device: AromaShooter) {
+  
   }
 }
 
